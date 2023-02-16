7706d578ec41ee6bfcc4c36bb5511c5ea54eb0c8
alec406 <ralec911@gmail.com>
2023-02-16 12:19:45 -0600

    Create initial project structure

---
+++ src/main/java/com/springboot/seahawksroster/SeahawksRosterApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.seahawksroster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeahawksRosterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeahawksRosterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/seahawksroster/SeahawksRosterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.seahawksroster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeahawksRosterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
