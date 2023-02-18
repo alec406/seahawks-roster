3d5d78f6fe2258d9f670a4718ce13a064170739a
alec406 <ralec911@gmail.com>
2023-02-18 16:38:01 -0600

    Configure DB

--- src/main/java/com/springboot/seahawksroster/SeahawksRosterApplication.java
+++ src/main/java/com/springboot/seahawksroster/SeahawksRosterApplication.java
@@ -3,7 +3,10 @@ package com.springboot.seahawksroster;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
 @SpringBootApplication
+@EnableEncryptableProperties
 public class SeahawksRosterApplication {
 
 	public static void main(String[] args) {
